9f6655fdb1c19b63aabaddb7adf65b11386caa0a
Paul <peiche@gmail.com>
2024-03-14 11:02:50 -0500

    Updated author to include link.

--- progress-block.php
+++ progress-block.php
@@ -5,7 +5,7 @@
  * Version:           1.0.0
  * Requires at least: 6.2
  * Requires PHP:      7.0
- * Author:            Paul Eiche
+ * Author:            <a href="https://eichefam.net">Paul Eiche</a>
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       peiche/progress-block
